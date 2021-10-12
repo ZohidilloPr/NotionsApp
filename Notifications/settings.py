6f399e1af8b568402af11a1df91c910fe614c1bf
zohidillopr <turgunovzohidillopr@yahoo.com>
2021-10-12 22:14:10 +0500

    bugs fixed

--- Notifications/settings.py
+++ Notifications/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -40,7 +42,7 @@ INSTALLED_APPS = [
     'App',
     "bootstrap5",
     'crispy_forms',
-            
+
 ]
 
 MIDDLEWARE = [
@@ -124,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILE=[
+STATICFILE = [
     os.path.join(BASE_DIR, '/static/')
 ]
 
